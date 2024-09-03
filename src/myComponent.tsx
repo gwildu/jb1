747dd929e0b8f4fbcc32fb4f4d486acabddd2a8f
Marius Rickenbacher <marius.rickenbacher@digitecgalaxus.ch>
2024-09-03 10:20:36 +0200

    simplifies example

--- src/myComponent.tsx
+++ src/myComponent.tsx
@@ -3,10 +3,9 @@ import {FunctionComponent} from "react";
 /**
  * MyComponent description
  * @param {string} something - Something description
- * @param {string} somethingElse - Something description
  * @returns {JSX.Element}
  * @constructor
  */
-export const MyComponent: FunctionComponent<{something: string, somethingElse: string}> = ({something, somethingElse}) => {
-    return <div>{something}{somethingElse}</div>;
+export const MyComponent: FunctionComponent<{something: string}> = ({something}) => {
+    return <div>{something}</div>;
 }
